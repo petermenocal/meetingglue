a7937e368cc20e9a3b176e356c3228cc1da98772
Peter Menocal <peter@geekypixel.com>
2019-06-14 16:43:13 -0700

    bug fixes and rss

--- routes/hotelsList.js
+++ routes/hotelsList.js
@@ -9,19 +9,33 @@ router.get("/", function(req, res, next) {
   var query = {};
   if (req.query.state) {
     query.state = req.query.state;
-  }
-  db.collection("hotels")
-    .find(query)
-    .sort({ title: 1 })
-    .toArray(function(err, result) {
-      if (err) throw Error;
-      var hotels = result;
-      res.render("hotelsList", {
-        title: "Hotels",
-        page_name: "hotels",
-        hotels: hotels
+    db.collection("hotels")
+      .find(query)
+      .sort({ title: 1 })
+      .toArray(function(err, result) {
+        if (err) throw Error;
+        var hotels = result;
+        res.render("hotelsList", {
+          title: "Hotels",
+          page_name: "hotels",
+          hotels: hotels,
+          state: query.state
+        });
       });
-    });
+  } else {
+    db.collection("hotels")
+      .find(query)
+      .sort({ title: 1 })
+      .toArray(function(err, result) {
+        if (err) throw Error;
+        var hotels = result;
+        res.render("hotelsList", {
+          title: "Hotels",
+          page_name: "hotels",
+          hotels: hotels
+        });
+      });
+  }
 });
 
 module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,25 @@
 var express = require("express");
 var router = express.Router();
 var localQuery = require("../utils/localQuery");
+let Parser = require("rss-parser");
 
 /* GET home page. */
 router.get("/", localQuery, function(req, res, next) {
-  res.render("home", { title: "Home", page_name: "home" });
+  let parser = new Parser();
+  var industryNews = [];
+  (async () => {
+    let feed = await parser.parseURL("http://smartmeetings.com/feed");
+    feed.items.forEach((item, idx) => {
+      if (idx < 5) {
+        industryNews.push(item);
+      }
+    });
+    res.render("home", {
+      title: "Home",
+      page_name: "home",
+      news: industryNews
+    });
+  })();
 });
 
 module.exports = router;
