7e7d6bdb95ccd07f1c4cb05ac86c6b7e4b09f729
Peter Menocal <peter@geekypixel.com>
2019-06-30 18:17:32 -0700

    accolades and protips sliders

---
+++ public/js/profile.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", function(event) {
+  var slider = tns({
+    container: ".protips",
+    items: 1,
+    slideBy: "page",
+    autoplay: true,
+    controlsPosition: "bottom"
+  });
+  var slider = tns({
+    container: ".accolades",
+    items: 1,
+    slideBy: "page",
+    autoplay: true,
+    controlsPosition: "bottom"
+  });
+});
