85f60db899f8884d68f52903d0ccb038108e2d13
Peter Menocal <peter@geekypixel.com>
2019-07-01 17:01:42 -0700

    update

--- routes/index.js
+++ routes/index.js
@@ -7,17 +7,36 @@ let Parser = require("rss-parser");
 router.get("/", localQuery, function(req, res, next) {
   let parser = new Parser();
   var industryNews = [];
+  var hotOpenings = [];
+  var featuredOpenings = [];
   (async () => {
-    let feed = await parser.parseURL("http://smartmeetings.com/feed");
+    let feed = await parser.parseURL(
+      "https://www.hospitalitynet.org/news/us.xml"
+    );
     feed.items.forEach((item, idx) => {
       if (idx < 5) {
         industryNews.push(item);
       }
     });
+
+    let openings = await parser.parseURL(
+      "https://www.hospitalitynet.org/news/openings.xml"
+    );
+    openings.items.forEach((item, idx) => {
+      if (idx < 8) {
+        hotOpenings.push(item);
+      }
+      if (idx > 7 && idx < 10) {
+        featuredOpenings.push(item);
+      }
+    });
+    console.log(hotOpenings);
     res.render("home", {
       title: "Home",
       page_name: "home",
-      news: industryNews
+      news: industryNews,
+      openings: hotOpenings,
+      featuredOpenings: featuredOpenings
     });
   })();
 });
